e02284f4d88c34fca982fe3319867afca24f0c2d
Balzamova <balzam0401@gmail.com>
2020-10-03 16:59:21 +0300

    feat: created functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array) {
+    return 0;
+  }
+  var count = array.length;
+  return count !== 0 ? Math.min(...array) : 0;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array) {
+    return 0;
+  }
+  var count = array.length;
+  return count !== 0 ? Math.max(...array) : 0;
+ 
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array) {
+    return 0;
+  }
+  var sum = 0,
+    count = 0;
+
+  for (var count = array.length, i = count - 1; i >= 0; i--) {
+    sum += array[i];
+  }
+  return count ? sum / count : 0;
 }
